4dd8fdc03e838ff57fdeebdf720d09df8848b62e
Romari00 <marioromariooo13@gmail.com>
2024-03-25 01:18:20 +0300

    <3

--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,11 @@
 import random
 import time
 
-from sqlalchemy import insert, select, exists
+from sqlalchemy import insert, select, exists, update
 from connection.db import Session, engine
 from telebot import types
 import telebot
-from models.user import User
+from models.user import User, Eco
 
 banword = ['nigger', 'крутой', 'прошмандовка Aruku', 'nigga', 'сучка', 'naga', 'cын булочника','приёмыш', 'подвальный ребёнок', 'ниггер', 'бездарь', 'нига', 'ушлёпок', 'уебан', 'тварь уродливая', 'нага', 'гомодрил', 'сын миража', 'faggot', 'шлюха', 'пидор', 'пидорас', 'педик', 'гомик', 'петух']
 
@@ -19,26 +19,19 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 
 def entered_users(message):
-
     with Session.begin() as session:
         user_have = session.query(exists().where(User.id == message.from_user.id)).scalar()
         if not user_have:
             users_dict = dict(id = message.from_user.id, name = message.from_user.first_name, username = message.from_user.username)
             query = insert(User).values(users_dict)
+            balance_dict = dict(id = message.from_user.id, balance = 100)
+            query1 = insert(Eco).values(balance_dict)
             session.execute(query)
+            session.execute(query1)
             session.commit()
+            bot.send_message(message.chat.id, 'Вы зарегистрировались')
         else:
             bot.send_message(message.chat.id, 'Вы уже зарегались')
-
-
-
-    # users_dict = dict(id = message.from_user.id+8, name = message.from_user.first_name, username = message.from_user.username)
-    # query = select(User.username).select_from(User)
-    # with Session.begin() as session:
-    #     t=session.execute(query).all()
-    # bot.send_message(message.chat.id,f'{t}')
-
-
 
 
 
@@ -51,30 +44,79 @@ def entered_users(message):
 
 @bot.message_handler(commands=['coinflip'])
 
-def coin_choice(message):
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add(types.KeyboardButton('Орёл'), types.KeyboardButton('Новосиб'))
-    bot.reply_to(message, "Выберите орёл или новосиб:", reply_markup=markup)
-    bot.register_next_step_handler(message, process_coin_choice)
+def bet(message):
+    global user
+    user = message.from_user.id
+    bot.reply_to(message, "Введите вашу ставку:")
+    bot.register_next_step_handler(message, process_bet)
+
+def process_bet(message):
+    global bet_amount, new_balance, user
+    if message.from_user.id == user:
+        bet_amount = int(message.text)
+        user = message.from_user.id
+
+        with Session.begin() as session:
+            query_balance = select(Eco.balance).select_from(Eco).where(Eco.id == user)
+            result = session.execute(query_balance).fetchone()
+            balance_amount = result[0]
+
+        if balance_amount < bet_amount:
+            bot.reply_to(message, "У вас недостаточно средств")
+            return
+        else:
+            new_balance = balance_amount - bet_amount
+            query_update_balance = update(Eco).values(balance = new_balance).where(Eco.id == user)
+            session.execute(query_update_balance)
+            session.commit()
+
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add(types.KeyboardButton('Орёл'), types.KeyboardButton('Новосиб'))
+        bot.reply_to(message, "Выберите орёл или новосиб:", reply_markup=markup)
+        bot.register_next_step_handler(message, process_coin_choice)
+
+# def edit_moralfuck_code(message):
+#     current_user = message.chat.id
+#     bet_amount = bet_amount = int(message.text)
+
 
 def process_coin_choice(message):
-    choice = message.text.lower()
-    if choice not in ['орёл', 'новосиб']:
-        bot.reply_to(message, "Пожалуйста, выберите орёл или новосиб.")
-        return
+    global new_balance, bet_amount
+    if message.from_user.id == user:
+        choice = message.text.lower()
+        if choice not in ['орёл', 'новосиб']:
+            bot.reply_to(message, "Пожалуйста, выберите орёл или новосиб.")
+            return
 
-    result = random.choice(['орёл', 'новосиб'])
-    img = open('media/coinflip.gif', 'rb')
-    sent_message = bot.send_animation(message.chat.id, img)
+        result = random.choice(['орёл', 'новосиб'])
+        img = open('media/coinflip.gif', 'rb')
+        sent_message = bot.send_animation(message.chat.id, img)
 
-    time.sleep(3)
+        time.sleep(3)
 
-    if choice == result:
-        bot.reply_to(message, f'Победа! Вы выбрали {choice}, результат: {result}.')
-    else:
-        bot.reply_to(message, f'Проебал! Вы выбрали {choice}, результат: {result}.')
+        if choice == result:
+            bot.reply_to(message, f'Победа! Вы выбрали {choice}, результат: {result}.')
+            with Session.begin() as session:
+                new_balance += (2*bet_amount)
+                query_update_balance = update(Eco).values(balance=new_balance).where(Eco.id == message.from_user.id)
+                session.execute(query_update_balance)
+                session.commit()
 
-    bot.delete_message(message.chat.id, sent_message.message_id)
+        else:
+            bot.reply_to(message, f'Проебал! Вы выбрали {choice}, результат: {result}.')
+
+        bot.delete_message(message.chat.id, sent_message.message_id)
+
+
+@bot.message_handler(commands=['balance'])
+def balance(message):
+    with Session.begin() as session:
+        query_balance = select(Eco.balance).select_from(Eco).where(Eco.id == message.from_user.id)
+        result = session.execute(query_balance).fetchone()
+        balance_amount = result[0]
+        session.commit()
+        bot.reply_to(message, f'Ваш баланс:  {balance_amount} ')
+
 
 
 
